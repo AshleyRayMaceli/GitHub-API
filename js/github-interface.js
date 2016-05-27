de98165a88c77ad7e954b69c164a159efb0a31d8
ashleyraymaceli <ashley.maceli@gmail.com>
2016-05-27 10:26:16 -0700

    update getRepos method to accept user input for a GitHub username, returns appropriate JSON information in the console

--- js/github-interface.js
+++ js/github-interface.js
@@ -2,8 +2,11 @@ var GitHub = require('./../js/github.js').GitHub;
 var getRepos = require('./../js/github.js').getRepos;
 
 $(document).ready(function() {
+  var newGitHubObject = new GitHub();
+
   $('#findUsername').click(function(){
-    var newGitHubObject = new GitHub();
-    newGitHubObject.getRepos();
+    var userName = $('#username').val();
+    $('#username').val("");
+    newGitHubObject.getRepos(userName);
   });
 });
--- js/github.js
+++ js/github.js
@@ -3,8 +3,9 @@ var apiKey = require('./../.env').apiKey;
 exports.GitHub = function() {
 }
 
-exports.GitHub.prototype.getRepos = function(){
-  $.get('https://api.github.com/users/daneden?access_token=' + apiKey).then(function(response){
+exports.GitHub.prototype.getRepos = function(ghusername){
+  $.get('https://api.github.com/users/' + ghusername +'?access_token=' + apiKey).then(function(response){
+    $('#output').text("Here is the info about " + ghusername + "'s GitHub account");
     console.log(JSON.stringify(response));
   }).fail(function(error){
     console.log(error.responseJSON.message);
