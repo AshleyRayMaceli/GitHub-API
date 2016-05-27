6c435e56f8b75d1cd00541775f315682775cd48b
ashleyraymaceli <ashley.maceli@gmail.com>
2016-05-27 10:51:50 -0700

    add displayGitHubInfo() function to interface.js and pass in through getRepos() to prevent asynchrony

--- js/github-interface.js
+++ js/github-interface.js
@@ -1,5 +1,10 @@
 var GitHub = require('./../js/github.js').GitHub;
 var getRepos = require('./../js/github.js').getRepos;
+// var displayGitHubInfo = require('./../js/github.js').displayGitHubInfo;
+
+var displayGitHubInfo = function(username) {
+  $('.output').text("Here is the info about " + username + "'s GitHub account");
+};
 
 $(document).ready(function() {
   var newGitHubObject = new GitHub();
@@ -7,6 +12,6 @@ $(document).ready(function() {
   $('#findUsername').click(function(){
     var userName = $('#username').val();
     $('#username').val("");
-    newGitHubObject.getRepos(userName);
+    newGitHubObject.getRepos(userName, displayGitHubInfo);
   });
 });
--- js/github.js
+++ js/github.js
@@ -1,11 +1,11 @@
 var apiKey = require('./../.env').apiKey;
 
 exports.GitHub = function() {
-}
+};
 
-exports.GitHub.prototype.getRepos = function(ghusername){
-  $.get('https://api.github.com/users/' + ghusername +'?access_token=' + apiKey).then(function(response){
-    $('#output').text("Here is the info about " + ghusername + "'s GitHub account");
+exports.GitHub.prototype.getRepos = function(ghusername, displayGitHubInfo) {
+  $.get('https://api.github.com/users/' + ghusername +'?access_token=' + apiKey).then(function(response) {
+    displayGitHubInfo(ghusername);
     console.log(JSON.stringify(response));
   }).fail(function(error){
     console.log(error.responseJSON.message);
