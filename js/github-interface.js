8e85043c659ad21164521a5b1c85fd766a80dc52
ashleyraymaceli <ashley.maceli@gmail.com>
2016-05-27 15:16:15 -0700

    add more styling, add show and hide functionality

--- js/github-interface.js
+++ js/github-interface.js
@@ -3,21 +3,26 @@ var getBasicUserInfo = require('./../js/github.js').getBasicUserInfo;
 var getAllRepos = require('./../js/github.js').getAllRepos;
 
 var displayGitHubInfo = function(username, repos, repoURL, profileURL) {
-  $('.output').html("Here is info about the GitHub account" + "<a href='" + profileURL + "'><h2>" + username + "</h2></a>");
-  $('.allRepos').text("Number of public repositories: " + repos);
-  $('.repoURL').text("View " + username + "'s repositories: " + repoURL);
+  $('.output').html("Here is info about the GitHub account" + "<a href='" + profileURL + "'><h2>" + username + "</h2></a>").show();
+  $('.allRepos').html("Number of public repositories: " + repos).show();
+  $('.repoURL').html("View " + username + "'s repositories: " + repoURL).show();
 };
 
 var displayErrorMessage = function(username) {
   $('.output').text(username + "'s GitHub account was not found.");
+  $('.allRepos').hide();
+  $('.repoURL').hide();
+  $('.repoInfo').hide();
 };
 
 var displayRepos = function(repoName, repoDescription, repoURL, dateCreated) {
-  $('.repoInfo').append("<div class='col-xs-4 repoBox'>" +
+  $('.repoInfo').append("<div class='col-xs-4'>" +
+                        "<div class='repoBox'>" +
                         "<a href='" + repoURL + "'><h2>" + repoName + "</h2></a>" +
                         "<h3> Created: " + moment(dateCreated).format("MM/DD/YYYY") + "</h3>" +
                         "<p>" + repoDescription + "</p>" +
-                        "<div>");
+                        "</div>" +
+                        "</div>").show();
 };
 
 $(document).ready(function() {
